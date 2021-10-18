35e874e33a25a1c004bb02c28ef2475b793481f2
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-10-18 17:40:54 +0530

    Adds Different Screens empty templates

---
+++ Screens/Shorts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Shorts = () => {
+  return (
+    <View>
+      <Text>Shorts</Text>
+    </View>
+  );
+};
+
+export default Shorts;
+
+const styles = StyleSheet.create({});
